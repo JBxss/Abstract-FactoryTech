44ce7f1ffb70b71925dd76c12f661e437ad9d029
JBxss <juanseb100@gmail.com>
2023-09-14 00:55:47 -0500

    [UPDATE] Readme.md y main view

--- Index.php
+++ Index.php
@@ -1,64 +1,66 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>FactoryTech</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <center>
-      <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        <h1>FactoryTech</h1>
-        <select name="Computadores">
-          <option>Computador de Mesa</option>
-          <option>Computador Portatil</option>
-          <option>Computador Todo en Uno</option>
-        </select>
-        <select name="Estilos">
-          <option>Gamer</option>
-          <option>Profesional</option>
-        </select>
-        <input type="submit" value="Comprar" />
-      </form>
-      <section>
-        <?php
 
-require "Clases.php";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<head>
+  <title>FactoryTech</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
 
-    $Tipo=$_POST['Computadores'];
-    $Estilo=$_POST['Estilos'];
+<body>
+  <center>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <h1>FactoryTech</h1>
+      <select name="Computadores">
+        <option>Computador de Mesa</option>
+        <option>Computador Portatil</option>
+        <option>Computador Todo en Uno</option>
+      </select>
+      <select name="Estilos">
+        <option>Gamer</option>
+        <option>Profesional</option>
+      </select>
+      <input type="submit" value="Comprar" />
+    </form>
+    <section>
+      <?php
 
-    if($Tipo=="Computador de Mesa" && $Estilo=="Gamer"){
-      echo "<img src='Assets/desktopGamer.webp'> <br>";
-        $Computadores= new FactoryDesktopGamer();
-    }
-    if($Tipo=="Computador de Mesa" && $Estilo=="Profesional"){
-      echo "<img src='Assets/7148191-800-auto.webp'> <br>";
-        $Computadores= new FactoryDesktopProfesional();
-    }
-    if($Tipo=="Computador Portatil" && $Estilo=="Gamer"){
-      echo "<img src='Assets/portatilGamer.jpg'> <br>";
-        $Computadores= new FactoryPortatilGamer();
-    }
-    if($Tipo=="Computador Portatil" && $Estilo=="Profesional"){
-      echo "<img src='Assets/portatil.jpg'> <br>";
-        $Computadores= new FactoryPortatilProfesional();
-    }
-    if($Tipo=="Computador Todo en Uno" && $Estilo=="Gamer"){
-      echo "<img src='Assets/AllGamer.png'> <br>";
-        $Computadores= new FactoryAllInOneGamer();
-    }
-    if($Tipo=="Computador Todo en Uno" && $Estilo=="Profesional"){
-      echo "<img src='Assets/all.webp'> <br>";
-        $Computadores= new FactoryAllInOneProfesional();
-    }
+      require "Clases.php";
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $Tipo = $_POST['Computadores'];
+        $Estilo = $_POST['Estilos'];
+
+        if ($Tipo == "Computador de Mesa" && $Estilo == "Gamer") {
+          echo "<img src='Assets/desktopGamer.webp'> <br>";
+          $Computadores = new FactoryDesktopGamer();
+        }
+        if ($Tipo == "Computador de Mesa" && $Estilo == "Profesional") {
+          echo "<img src='Assets/7148191-800-auto.webp'> <br>";
+          $Computadores = new FactoryDesktopProfesional();
+        }
+        if ($Tipo == "Computador Portatil" && $Estilo == "Gamer") {
+          echo "<img src='Assets/portatilGamer.jpg'> <br>";
+          $Computadores = new FactoryPortatilGamer();
+        }
+        if ($Tipo == "Computador Portatil" && $Estilo == "Profesional") {
+          echo "<img src='Assets/portatil.jpg'> <br>";
+          $Computadores = new FactoryPortatilProfesional();
+        }
+        if ($Tipo == "Computador Todo en Uno" && $Estilo == "Gamer") {
+          echo "<img src='Assets/AllGamer.png'> <br>";
+          $Computadores = new FactoryAllInOneGamer();
+        }
+        if ($Tipo == "Computador Todo en Uno" && $Estilo == "Profesional") {
+          echo "<img src='Assets/all.webp'> <br>";
+          $Computadores = new FactoryAllInOneProfesional();
+        }
 
 
-    $Clase= new EnsamblajeComputadores($Computadores);
+        $Clase = new EnsamblajeComputadores($Computadores);
+      }
+      ?>
+    </section>
+  </center>
+</body>
 
-    }
-    ?>
-      </section>
-    </center>
-  </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# AbstractFactoryPHP
-Demostracion de Abstract factory en PHP en una fabrica Tech
+# Abstract FactoryTech
+
+## Descripción
+
+Abstract FactoryTech es una fábrica de tecnología desarrollada en PHP que utiliza el patrón de diseño Abstract Factory para la creación de computadoras personalizadas. Este proyecto permite a los usuarios seleccionar el tipo de computadora que desean y genera una imagen que representa la fabricación de la PC basada en esa elección.
+
+## Capturas de Pantalla
+
+![Página de Inicio](./Assets/screenshots/screenshot.png)
+*Página de Inicio de FactoryTech*
+
+## Funcionalidades
+
+- **Selección de Tipo de Computadora**: Los usuarios pueden elegir entre dos tipos de computadoras: "Gamer" o "Profesional".
+
+- **Generación de Imagen**: Una vez seleccionado el tipo de computadora, la aplicación generará una imagen que muestra el proceso de fabricación de la computadora seleccionada.
+
+## Patrón de Diseño Abstract Factory
+
+Abstract FactoryTech es un ejemplo de implementación del patrón de diseño Abstract Factory. Este patrón permite la creación de familias de objetos relacionados sin especificar sus clases concretas. En este caso, la fábrica abstracta crea componentes de computadora (como CPU, GPU, RAM, etc.) según la categoría seleccionada.
+
+## Ejemplo de Uso
+
+```php
+// Crear una fábrica para computadoras gamer
+$gamerFactory = new GamerComputerFactory();
+$gamerComputer = $gamerFactory->createComputer();
+
+// Crear una fábrica para computadoras profesionales
+$professionalFactory = new ProfessionalComputerFactory();
+$professionalComputer = $professionalFactory->createComputer();
+```
+
+## Requisitos
+
+- PHP 7.0 o superior.
+- Servidor web (por ejemplo, Apache) para ejecutar la aplicación.
+
+## Instalación
+
+1. Clona este repositorio en tu servidor web local o en tu servidor de producción:
+
+   ```bash
+   git clone https://github.com/JBxss/Abstract-FactoryTech.git
+   ```
+
+2. Configura tu servidor web para que el directorio raíz apunte a la carpeta donde clonaste el repositorio.
+
+3. Abre un navegador web y accede a la URL de tu servidor para comenzar a usar Abstract FactoryTech.
+
+---
+
+¡Gracias por usar Abstract FactoryTech! Esperamos que esta fábrica de tecnología te ayude a comprender mejor el patrón de diseño Abstract Factory y a crear computadoras personalizadas de forma sencilla. Si tienes preguntas o problemas, no dudes en abrir un problema o enviar una solicitud de extracción. ¡Disfruta de la creación de computadoras personalizadas!
